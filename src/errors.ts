b3a068111e256ece744cbfbd4431ce8b15f1ffce
Onasis English <onasisvlicon@gmail.com>
2026-06-05 11:11:56 -0600

    added questions errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,16 +1,27 @@
 export type ErrorId =
   | "double_subject"
+
+  // Present simple
   | "present_simple_third_person_s"
   | "present_simple_third_person_doesnt"
   | "missing_dont_doesnt"
   | "doesnt_with_s_verb"
+
+  // To be
   | "wrong_be_form"
   | "missing_be"
   | "be_with_do"
   | "wrong_be_negation"
+
+  // Generic
   | "missing_object"
   | "missing_negation"
-  | "unexpected_negation";
+  | "unexpected_negation"
+
+  // Questions
+  | "missing_question_auxiliary"
+  | "question_word_order"
+  | "question_with_conjugated_main_verb";
 
 export type EngineError = {
   errorId: ErrorId;
