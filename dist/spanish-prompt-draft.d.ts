1637e0d2c0f104c3197e3aed7d12432d9904c6dc
Onasis English <onasisvlicon@gmail.com>
2026-06-05 08:54:57 -0600

    add sentenceType to scene contracts

--- dist/spanish-prompt-draft.d.ts
+++ dist/spanish-prompt-draft.d.ts
@@ -2,6 +2,7 @@ import { Scene } from "./scene";
 export type SpanishPromptDraft = {
     tense: Scene["tense"];
     polarity: Scene["polarity"];
+    sentenceType: Scene["sentenceType"];
     subject: string;
     verb: string;
     englishSentence: string;
--- src/spanish-prompt-draft.ts
+++ src/spanish-prompt-draft.ts
@@ -1,13 +1,15 @@
 import { Scene } from "./scene";
 
 export type SpanishPromptDraft = {
-    tense: Scene["tense"];
-  
-    polarity: Scene["polarity"];
-  
-    subject: string;
-  
-    verb: string;
-  
-    englishSentence: string;
-  };
+  tense: Scene["tense"];
+
+  polarity: Scene["polarity"];
+
+  sentenceType: Scene["sentenceType"];
+
+  subject: string;
+
+  verb: string;
+
+  englishSentence: string;
+};
